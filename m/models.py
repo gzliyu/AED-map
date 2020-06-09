b5173ae81bc427af49ea424783847cb1d0428f68
gzliyu <gzliyu@users.noreply.github.com>
2020-06-09 22:11:23 +0800

    通信接口，前端更多的customize，数据库

--- m/models.py
+++ m/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Flight(models.Model):
+    origin = models.CharField(max_length=64)
+    destination = models.CharField(max_length=64)
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return '{id}: {origin} to {destination} {duration}min'.format(
+            id=self.id,
+            origin=self.origin,
+            destination=self.destination,
+            duration=self.duration)
+
+
+class Point(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    description = models.TextField()
+
+    def __str__(self):
+        return '{id}: {description}  [{latitude}, {longitude}]'.format(
+            id=self.id,
+            description=self.description,
+            latitude=self.latitude,
+            longitude=self.longitude)
--- m/views.py
+++ m/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Point
+
 import json
 from datetime import datetime
 # Create your views here.
@@ -9,7 +11,26 @@ def index(request):
     return HttpResponse("This module is designed for http message.")
 
 
+def distance(pos, Point):
+    '''
+    select的辅助函数
+    '''
+    return abs(Point.latitude - pos[0]) + abs(Point.longitude - pos[1])
+
+
+def select(pos, Points, num):
+    '''
+    选出距离已知点最近的num个点
+    :param pos:[Lat,Lng]
+    :param Points:list Lat,Lng,description
+    :param num:排序参数
+    '''
+    Points.sort(key=distance)
+    return Points[:num]
+
+
 def process(request, get):
+    print(request)
     getmessage = json.dumps(get)
     t = datetime.now().timestamp()
     # 验证不成功或者其他错误情况
@@ -22,11 +43,18 @@ def process(request, get):
         return HttpResponse(message)
     else:
         # 处理请求
-        message = json.dumps({
+
+        message = {
             'success': True,
             'time': t,
-            'data': {
-
-            },
-        })
-        return HttpResponse(message)
+            'objects': []
+        }
+        for point in Point.objects.all():
+            message['objects'].append({
+                'Place': point.description,
+                'Lat': point.latitude,
+                'Lnt': point.longitude
+            })
+        print(message['success'])
+        return HttpResponse(json.dumps(message, ensure_ascii=False),
+                            content_type="application/json,charset=utf-8")
